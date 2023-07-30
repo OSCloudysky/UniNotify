b5703555d30c2d5c0cad5d10ff93316f926079c2
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-07-30 07:09:44 -0400

    add github execution logs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import * as service from './services'
+import {Octokit} from '@octokit/rest'
 
 async function run(): Promise<void> {
-  const message = core.getInput('message')
   const messageType = core.getInput('messageType')
+  const githubToken = core.getInput('githubToken')
+
+  // Get more details information of the job
+  const context = github.context
+  const octokit = new Octokit({auth: `token ${githubToken}`})
+
+  const commit = await octokit.repos.getCommit({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    ref: context.sha
+  })
+
+  const message = `
+    Workflow: ${context.workflow} 
+    Job: ${context.job}
+    Status: ${context.payload.action}
+    Commit Message: ${commit.data.commit.message}
+    Commit URL: https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}
+    Event: ${context.eventName}
+    Runner: ${context.runId}
+    Ref: ${context.ref}
+  `
+
   try {
     const token = core.getInput('slackToken')
     if (messageType === 'slack') {
